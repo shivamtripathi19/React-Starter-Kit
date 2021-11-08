f620d4188619f0b65d06fdc4c7ba879fd568a12c
shivamtripathi19 <shivam.tripathi@tokopedia.com>
2021-11-08 13:07:12 +0530

    react-starter-kit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './index.css';
+
+class App extends React.Component {
+    render() {
+      return (<div>Hello </div>)
+    }
+  }
+  
+export default App; 
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const HTMlWebpackPlugin = require("html-webpack-plugin")
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        use: "babel-loader",
+        
+      },
+      {
+        test: /\.(css)$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+
+  mode: "development",
+ plugins: [new HTMlWebpackPlugin({
+    template:'public/index.html'
+  })]
+};
